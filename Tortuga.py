e31e28e41be410c2406ed76e5ac5bd72e1269d39
sonysarkis <sonygomez6@gmail.com>
2024-10-31 14:02:37 -0400

    Variables renombradas

--- Tortuga.py
+++ Tortuga.py
@@ -1,70 +1,70 @@
 import turtle
 
-# dibuja un triángulo dado un conjunto de puntos, un color y un objeto Turtle.
-def dibujarTriangulo(puntos, color, miTortuga):
+#triángulo dado un conjunto de vértices, un color y un objeto Turtle.
+def dibujar_triangulo(vertices, color, tortuga):
     # color de relleno del triángulo
-    miTortuga.fillcolor(color)
-    # lápiz a la primera coordenada del triángulo sin dejar rastro
-    miTortuga.up()
-    miTortuga.goto(puntos[0][0], puntos[0][1])
-    # Baja el lápiz para empezar a dibujar
-    miTortuga.down()
-    # rellenar la figura con el color especificado
-    miTortuga.begin_fill()
-    # triángulo conectando los tres puntos dados
-    miTortuga.goto(puntos[1][0], puntos[1][1])
-    miTortuga.goto(puntos[2][0], puntos[2][1])
-    miTortuga.goto(puntos[0][0], puntos[0][1])
+    tortuga.fillcolor(color)
+    # lápiz para moverlo al primer vértice sin dibujar
+    tortuga.up()
+    tortuga.goto(vertices[0][0], vertices[0][1])
+    # lápiz para comenzar a dibujar
+    tortuga.down()
+    # relleno del triángulo
+    tortuga.begin_fill()
+    # triángulo conectando los tres vértices
+    tortuga.goto(vertices[1][0], vertices[1][1])
+    tortuga.goto(vertices[2][0], vertices[2][1])
+    tortuga.goto(vertices[0][0], vertices[0][1])
     # fin del relleno del triángulo
-    miTortuga.end_fill()
+    tortuga.end_fill()
 
-# Esta función obtiene el punto medio entre dos puntos dados.
-def obtenerMitad(p1, p2):
+# Calcula el punto medio entre dos puntos dados.
+def obtener_punto_medio(punto1, punto2):
     # Calcula las coordenadas del punto medio en el eje X e Y
-    return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
+    return ((punto1[0] + punto2[0]) / 2, (punto1[1] + punto2[1]) / 2)
 
 # Función recursiva para crear el triángulo de Sierpinski.
-# Cada nivel de recursión reduce el grado y divide el triángulo en partes más pequeñas.
-def sierpinski(puntos, grado, miTortuga):
-    # Definimos una lista de colores para cada nivel de profundidad
-    colormap = ['purple', 'cyan', 'lime', 'pink', 'gold', 'orange', 'aqua']
-    # Dibuja un triángulo con el color correspondiente al grado de profundidad actual
-    dibujarTriangulo(puntos, colormap[grado], miTortuga)
+# Cada nivel de recursión reduce la profundidad y subdivide el triángulo en partes más pequeñas.
+def sierpinski(vertices, profundidad, tortuga):
+    # lista de colores para cada nivel de profundidad
+    colores = ['purple', 'cyan', 'lime', 'pink', 'gold', 'orange', 'aqua']
+    # triángulo con el color correspondiente al nivel de profundidad actual
+    dibujar_triangulo(vertices, colores[profundidad], tortuga)
     
-    # Si el grado es mayor que 0, continuamos dividiendo el triángulo en subtriángulos
-    if grado > 0:
+    # Si la profundidad es mayor que 0, continúa subdividiendo el triángulo
+    if profundidad > 0:
         # Llamada recursiva para el triángulo superior izquierdo
-        sierpinski([puntos[0], 
-                    obtenerMitad(puntos[0], puntos[1]), 
-                    obtenerMitad(puntos[0], puntos[2])],
-                   grado - 1, miTortuga)
+        sierpinski([vertices[0], 
+                    obtener_punto_medio(vertices[0], vertices[1]), 
+                    obtener_punto_medio(vertices[0], vertices[2])],
+                   profundidad - 1, tortuga)
         
         # Llamada recursiva para el triángulo superior derecho
-        sierpinski([puntos[1], 
-                    obtenerMitad(puntos[0], puntos[1]), 
-                    obtenerMitad(puntos[1], puntos[2])],
-                   grado - 1, miTortuga)
+        sierpinski([vertices[1], 
+                    obtener_punto_medio(vertices[0], vertices[1]), 
+                    obtener_punto_medio(vertices[1], vertices[2])],
+                   profundidad - 1, tortuga)
         
         # Llamada recursiva para el triángulo inferior
-        sierpinski([puntos[2], 
-                    obtenerMitad(puntos[2], puntos[1]), 
-                    obtenerMitad(puntos[0], puntos[2])],
-                   grado - 1, miTortuga)
+        sierpinski([vertices[2], 
+                    obtener_punto_medio(vertices[2], vertices[1]), 
+                    obtener_punto_medio(vertices[0], vertices[2])],
+                   profundidad - 1, tortuga)
 
 # Función principal que inicializa la pantalla y configura los parámetros iniciales.
 def main():
     # Crea el objeto Turtle para dibujar
-    miTortuga = turtle.Turtle()
-    # Crea la ventana donde se visualizará el dibujo
-    miVentana = turtle.Screen()
+    tortuga = turtle.Turtle()
+    # ventana donde se visualizará el dibujo
+    ventana = turtle.Screen()
     
-    # Define los puntos del triángulo inicial, modificados para cambiar la forma del triángulo
-    misPuntos = [[-120, -80], [0, 120], [120, -80]]
+    # Define los vértices del triángulo inicial para la forma deseada
+    vertices_iniciales = [[-120, -80], [0, 120], [120, -80]]
     # Llama a la función sierpinski para comenzar a dibujar el triángulo de Sierpinski
-    sierpinski(misPuntos, 3, miTortuga)
+    sierpinski(vertices_iniciales, 3, tortuga)
     
-    # Mantiene la ventana abierta hasta que se haga clic
-    miVentana.exitonclick()
+    # ventana abierta hasta que se haga clic
+    ventana.exitonclick()
 
-# Llama a la función principal para ejecutar el programa
 main()
+
